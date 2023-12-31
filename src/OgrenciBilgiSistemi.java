7fa40b21ebd59b1015ba7f3c899e94b53b2b57e4
geldi <geldiyigit1@gmail.com>
2023-12-31 18:16:57 +0300

    abstact class ve derslerde kalıtım ekledim

--- src/OgrenciBilgiSistemi.java
+++ src/OgrenciBilgiSistemi.java
@@ -1,32 +1,75 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+abstract class OrtakDersler {
+    List<Ders> dersler;
 
-class OgrenciBilgiSistemi {
-    static class Ders {
-        int ders_kodu;
-        String ders_ismi;
-        int kayit_sayisi;
-
-        public Ders(int ders_kodu, String ders_ismi) {
-            this.ders_kodu = ders_kodu;
-            this.ders_ismi = ders_ismi;
-            this.kayit_sayisi = 0;
-        }
+    public OrtakDersler() {
+        this.dersler = new ArrayList<>();
+        dersler.add(new Ders(101, "Türk Dili"));
+        dersler.add(new Ders(102, "Matematik"));
+        dersler.add(new Ders(103, "Fizik"));
+        dersler.add(new Ders(104, "Atatürk İlkeleri ve İnkılap Tarihi"));
     }
 
+
+    public abstract void dersEkle(Ders yeniDers);
+
+}
+
+
+class Yazilim extends OrtakDersler {
+    public Yazilim() {
+        super();
+        dersler.add(new Ders(201, "Algoritma"));
+        dersler.add(new Ders(202, "Nesne"));
+        dersler.add(new Ders(203, "Gereksinim"));
+        dersler.add(new Ders(204, "WEB"));
+    }
+
+    @Override
+    public void dersEkle(Ders yeniDers) {
+        dersler.add(yeniDers);
+    }
+}
+
+class Mekatronik extends OrtakDersler {
+    public Mekatronik() {
+        super();
+        dersler.add(new Ders(301, "Mekatronik Mühendisliğine Giriş"));
+        dersler.add(new Ders(302, "Elektrik Devreleri"));
+        dersler.add(new Ders(303, "Bilgisayar Destekli Teknik Resim"));
+    }
+
+    @Override
+    public void dersEkle(Ders yeniDers) {
+        dersler.add(yeniDers);
+    }
+}
+class Ders {
+    int ders_kodu;
+    String ders_ismi;
+    int kayit_sayisi;
+
+    public Ders(int ders_kodu, String ders_ismi) {
+        this.ders_kodu = ders_kodu;
+        this.ders_ismi = ders_ismi;
+        this.kayit_sayisi = 0;
+    }
+}
+
+
+public class OgrenciBilgiSistemi {
     static class Ogrenci {
         int ogrenci_numarasi;
         String isim;
         String soyisim;
+        int bolum;
         List<String> alinan_dersler;
 
         public Ogrenci(int ogrenci_numarasi, String isim, String soyisim) {
             this.ogrenci_numarasi = ogrenci_numarasi;
             this.isim = isim;
             this.soyisim = soyisim;
+            this.bolum = bolum;
             this.alinan_dersler = new ArrayList<>();
         }
     }
@@ -39,92 +82,103 @@ class OgrenciBilgiSistemi {
             this.dersler = new HashMap<>();
             this.ogrenciler = new HashMap<>();
 
-            // Öğrencileri başlangıçta tanımla
-            ogrenci_ekle(1, "Eren", "Özcan");
-            ogrenci_ekle(2, "Yiğit", "Geldi");
-            ogrenci_ekle(3, "Onur", "Malay");
-            ogrenci_ekle(4, "Osman", "Güzel");
-            ogrenci_ekle(5, "Tuğba", "Çelikten");
+            // Dersleri tanımla
+            Yazilim yazilimDersleri = new Yazilim();
+            Mekatronik mekatronikDersleri = new Mekatronik();
 
-            // Kursları başlangıçta tanımla
-            ders_ekle(101, "Kalkülüs1");
-            ders_ekle(102, "Kalkülüs2");
-            ders_ekle(103, "Fizik1");
-            ders_ekle(104, "Fizik2");
-            ders_ekle(105, "Algoritma1");
-            ders_ekle(106, "Algoritma2");
-            ders_ekle(107, "Nesne");
-            ders_ekle(108, "Gereksinim");
-            ders_ekle(109, "WEB");
-            ders_ekle(110, "İş Sağlığı");
+            for (Ders ders : yazilimDersleri.dersler) {
+                dersler.put(ders.ders_kodu, ders);
+            }
+            for (Ders ders : mekatronikDersleri.dersler) {
+                dersler.put(ders.ders_kodu, ders);
+            }
+
+            // Öğrencileri tanımla
+            ogrenci_ekle(1, "Eren", "Özcan", 1);
+            ogrenci_ekle(2, "Yiğit", "Geldi", 1);
+            // Diğer öğrencileri ekle...
         }
 
-        public void ders_ekle(int ders_kodu, String ders_ismi) {
-            Ders ders = new Ders(ders_kodu, ders_ismi);
-            dersler.put(ders_kodu, ders);
-        }
-
-        public void ogrenci_ekle(int ogrenci_numarasi, String isim, String soyisim) {
+        public void ogrenci_ekle(int ogrenci_numarasi, String isim, String soyisim,  int bolum) {
             Ogrenci ogrenci = new Ogrenci(ogrenci_numarasi, isim, soyisim);
             ogrenciler.put(ogrenci_numarasi, ogrenci);
         }
 
-        public void DersListesiniGoruntule() {
+        public void DersListesiniGoruntule(int bolum) {
             System.out.println("Mevcut Kurslar:");
-            for (Ders ders : dersler.values()) {
+
+            List<Ders> gosterilecekDersler;
+            if (bolum == 1) { // Yazılım Bölümü
+                gosterilecekDersler = new Yazilim().dersler;
+            } else if (bolum == 2) { // Mekatronik Bölümü
+                gosterilecekDersler = new Mekatronik().dersler;
+            } else {
+                System.out.println("Geçersiz bölüm seçimi!");
+                return;
+            }
+
+            for (Ders ders : gosterilecekDersler) {
                 System.out.println(ders.ders_kodu + ": " + ders.ders_ismi +
                         " - Kayıtlı öğrenciler: " + ders.kayit_sayisi);
             }
         }
+
 
         public void OgrenciKaydetme(int ogrenci_numarasi, int ders_kodu) {
             if (ogrenciler.containsKey(ogrenci_numarasi) && dersler.containsKey(ders_kodu)) {
                 Ogrenci ogrenci = ogrenciler.get(ogrenci_numarasi);
                 Ders ders = dersler.get(ders_kodu);
 
-                ogrenci.alinan_dersler.add(ders.ders_ismi);
-                ders.kayit_sayisi++;
-
-                System.out.println(ogrenci.isim + " başarı ile " + ders.ders_ismi + "'e kaydolmuştur");
+                if (!ogrenci.alinan_dersler.contains(ders.ders_ismi)) {
+                    ogrenci.alinan_dersler.add(ders.ders_ismi);
+                    ders.kayit_sayisi++;
+                    System.out.println(ogrenci.isim + " başarı ile " + ders.ders_ismi + "'e kaydolmuştur");
+                } else {
+                    System.out.println(ogrenci.isim + " zaten " + ders.ders_ismi + " dersine kayıtlı");
+                }
             } else {
                 System.out.println("Geçersiz öğrenci ya da ders kodu");
             }
         }
 
         public void IstasitikleriGoruntule() {
-            System.out.println("Ders kayıt durumu");
+            System.out.println("Ders kayıt istatistikleri:");
             for (Ders ders : dersler.values()) {
-                System.out.println(ders.ders_ismi + " - kayıtlı öğrenci sayısı " + ders.kayit_sayisi);
+                System.out.println(ders.ders_ismi + " - Kayıtlı öğrenci sayısı: " + ders.kayit_sayisi);
             }
         }
     }
 
     public static void main(String[] args) {
-        DersEklemeCikarma DersEklemeCikarma = new DersEklemeCikarma();
+
+        DersEklemeCikarma dersEklemeCikarma = new DersEklemeCikarma();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-
             // Kullanıcı girişi
-            System.out.print("öğrenci numarası giriniz: ");
+            System.out.print("Öğrenci numarasını giriniz: ");
             int ogrenci_numarasi = scanner.nextInt();
 
+            System.out.print("Hangi bölümde okuyorsunuz? (1 - Yazılım Mühendisliği, 2 - Mekatronik): ");
+            int bolum = scanner.nextInt();
+
             // Kurs listesini göster
-            DersEklemeCikarma.DersListesiniGoruntule();
+            dersEklemeCikarma.DersListesiniGoruntule(bolum);
 
             // Kurs seçimi
             System.out.print("Kayıt olmak istediğiniz dersin kodunu giriniz (çıkış için 0 girin): ");
             int ders_kodu = scanner.nextInt();
 
             if (ders_kodu == 0) {
+                dersEklemeCikarma.IstasitikleriGoruntule();
                 break;
             }
 
             // Öğrenciyi kursa kaydet
-            DersEklemeCikarma.OgrenciKaydetme(ogrenci_numarasi, ders_kodu);
+            dersEklemeCikarma.OgrenciKaydetme(ogrenci_numarasi, ders_kodu);
         }
 
         // İstatistikleri göster
-        DersEklemeCikarma.IstasitikleriGoruntule();
+        dersEklemeCikarma.IstasitikleriGoruntule();
     }
 }
